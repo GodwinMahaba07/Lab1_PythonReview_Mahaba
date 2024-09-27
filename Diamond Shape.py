969f82f58cb73df2ef66539a42ad73a4376acbd2
GodwinMahaba07 <163639188+GodwinMahaba07@users.noreply.github.com>
2024-09-27 13:13:07 +0800

    Add files via upload

---
+++ Diamond Shape.py
@@ -0,0 +1,18 @@
+
+#Godwin Anton M. Mahaba
+#Bscpe 2-2
+#Exercise 3: Diamond Shape
+
+
+n = int(input("Enter an odd number: "))
+
+if n % 2 == 0:
+    print("Please provide an odd integer.")
+else:
+    # Top half of the diamond
+    for i in range(n // 2 + 1):
+        print(' ' * (n // 2 - i) + '*' * (2 * i + 1))
+
+    # Bottom half of the diamond
+    for i in range(n // 2 - 1, -1, -1):
+        print(' ' * (n // 2 - i) + '*' * (2 * i + 1))
